955cb5dec81de47e0990e4d8c37f5f08162f9ca7
h-yki <yuuki531259@gmail.com>
2023-06-06 14:20:34 +0900

    Last update 2023/6/6 14:20

--- src/todo.js
+++ src/todo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './todo.css';
 export default class Todo extends Component {
     constructor(props){
         super(props);
@@ -64,32 +65,56 @@ export default class Todo extends Component {
 
     render(){
         const{todos, finished, count} = this.state;
-        return(<body>
-            <h1>Todo管理アプリ</h1>
-            <form name="elem">
-            <input type = "text" placeholder='例: 計算機アーキテクチャ' onInput={this.onInput} size='30' />
+        
+        return(
+        <body>
+            <h1 className='headtitle'>Todo管理アプリ</h1>
+            <form name="elem" className='inputelem'>
+                <input type = "text" placeholder='例: 計算機アーキテクチャ' onInput={this.onInput} className='input' />
             </form>
-            <button onClick={this.addTodo}>登録</button>
-            <ul id='currentTask'>現在のタスク
-                {todos.map((todo, index) => 
-                    <li key ={index}>
-                        {todo}
-                        <button onClick={() => {this.removeTodo(index)}}>完了</button>
-                    </li>
-                )}
-            </ul>
-            <ul id='finishedTask'>完了タスク
-            {finished.map((finished, index) => 
-                    <li key ={index}>
-                        {finished}
-                        <button onClick={() => {this.removefinish(index)}}>削除</button>
-                        <button onClick={() => {this.replacetask(index)}}>戻す</button>
-                    </li>
-                )}
-            </ul>
-            <h3>あなたは今までに{count}個のタスクを完了しました！</h3>
-            <button onClick={() => {this.Allreset()}}>タスクを全てリセット</button>
-            </body>
+            <div className='addtodo' align="center">
+                <button onClick={this.addTodo}>登録</button>
+            </div>
+
+            <div className='body-container'>
+                <div className='currenttask-container'>
+                    <h4 className='currenttask'>現在のタスク</h4>
+                    <ul id='currentTask'>
+                        {todos.map((todo, index) => 
+                         <li key ={index} className='currenttodo'>
+                            {todo}
+                         <button className='finish' onClick={() => {this.removeTodo(index)}}>完了</button>
+                        </li>
+                        )}
+                    </ul>
+                </div>
+                <div className='finishedtask-container'>
+                    <h4 className='finishedtask'>完了タスク</h4>
+                    <ul id='finishedTask'>
+                        {finished.map((finished, index) => 
+                            <li key ={index}>
+                                {finished}
+                                <button className='delete' onClick={() => {this.removefinish(index)}}>削除</button>
+                                <button onClick={() => {this.replacetask(index)}}>戻す</button>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+
+            <h3 className='count'>あなたは今までに{count}個のタスクを完了しました！</h3>
+            <div className='allreset' align="right">
+                <button onClick={() => {this.Allreset()}}>タスクを全てリセット</button>
+            </div>
+
+            <footer>
+                <p>This page is enPiT2023 production by h-yki.</p>
+                <div className='links'>
+                    <a href='https://github.com/h-yki/TodoApp.git' target='_blank'>Github</a>
+                    <a href='https://scrapbox.io/enpit2023/%E5%8E%9F%E3%81%A1%E3%82%83%E3%82%93' target='_blank'>scrapbox</a>
+                </div>
+            </footer>
+        </body>
         );
     }
 }
